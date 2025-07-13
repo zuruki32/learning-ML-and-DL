22bb2009d100558e86a47a11e10eded64425d864
melal9057 <masisadeghi78@gmail.com>
2025-07-13 09:06:02 +0330

    Remove obsolete files

--- ML-DL/HierarchicalClusteringMarket.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import scipy.cluster.hierarchy as cluster_algorithm
-from sklearn.cluster import AgglomerativeClustering
-
-shopping_data = pd.read_csv('shopping_data.csv')
